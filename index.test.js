2558542e91b3f9293512ccd4f4d42ed57a8e4048
Silver Gama <silver.mdg@gmail.com>
2019-05-29 23:23:09 -0300

    add unit test

---
+++ index.test.js
@@ -0,0 +1,13 @@
+const test = require('tape')
+const index = require('./index')
+
+test( 'Aplicar desconto', (t) => {
+    t.assert( index.aplicarDesconto(10, 5) === 5, 'Descontou corretamente')
+    t.end()
+})
+
+test( 'Aplicar deconto grande', (t) => {
+    t.assert( index.aplicarDesconto(5, 10) === 0 , 'Descontou corretamente')
+    t.end()
+})
+
