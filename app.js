5e907fa3982fcbfcd432b2cc2f0ea43aea6ea038
Silver Gama <silver.mdg@gmail.com>
2019-06-01 01:10:50 -0300

    Add integration test

---
+++ app.js
@@ -0,0 +1,27 @@
+const index = require('./index')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = 3000
+
+// Configurando Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Configurando Rotas
+const router  = express.Router();
+router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+
+router.get('/aplicarDesconto/:valor/:desconto', (req, res) => {
+    const valor = parseInt(req.params.valor)
+    const desconto = parseInt(req.params.desconto)
+    res.json({valorDescontado: index.aplicarDesconto(valor,desconto)})
+    })
+
+app.use( '/', router)
+if(require.main === module ){
+    app.listen(port)
+    console.log('API Funcionando');
+}
+
+module.exports = app
---
+++ tests/app.test.js
@@ -0,0 +1,17 @@
+
+const test = require('tape')
+const supertest = require('supertest')
+const app = require('../app')
+
+test('GET /aplicarDesconto/10/5', (t) => {
+    supertest(app)
+    .get('/aplicarDesconto/10/5')
+    .expect('Content-Type', /json/)
+    .expect(200)
+    .end( (err, res) => {
+        
+        t.error(err, 'Sem erros')
+        t.assert(res.body.valorDescontado === 5, 'Desconto Correto')
+        t.end()
+    })
+})
