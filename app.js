e4b7dd1fa104921b25c7ab12fbcac998e9fc1d17
Silver Gama <silver.mdg@gmail.com>
2019-07-04 11:59:43 -0300

    Update app.js
    test-jenkins

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(bodyParser.json());
 const router  = express.Router();
 router.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 
+router.get('/test', (req, res) => res.json({ message: 'Route Test Okay!' }));
+
 router.get('/aplicarDesconto/:valor/:desconto', (req, res) => {
     const valor = parseInt(req.params.valor)
     const desconto = parseInt(req.params.desconto)
